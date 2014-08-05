3f0bbe9b4bfca49e84810399326afb7aa3dfbd25
dingjb <dingjb.yahoo.com>
2014-08-05 16:54:56 -0400

    -OutputDirectory .eT\eT\code\packages

--- eT/code/NugetExeInstallPackage.ps1
+++ eT/code/NugetExeInstallPackage.ps1
@@ -1,7 +1,7 @@
-nuget.exe config
-Nuget.exe install .eT\eT\code\Shell\packages.config
-Nuget.exe install .eT\eT\code\Trading\packages.config
-Nuget.exe install .eT\eT\code\Modules\packages.config
-Nuget.exe install .eT\eT\code\DataGridWithColumnStyle\packages.config
-Nuget.exe install .eT\eT\code\p1\packages.config
+#nuget.exe config
+Nuget.exe install .eT\eT\code\Shell\packages.config -OutputDirectory .eT\eT\code\packages
+Nuget.exe install .eT\eT\code\Trading\packages.config -OutputDirectory .eT\eT\code\packages
+Nuget.exe install .eT\eT\code\Modules\packages.config -OutputDirectory .eT\eT\code\packages
+Nuget.exe install .eT\eT\code\DataGridWithColumnStyle\packages.config -OutputDirectory .eT\eT\code\packages
+Nuget.exe install .eT\eT\code\p1\packages.config -OutputDirectory .eT\eT\code\packages
 #Start-Sleep -s 600

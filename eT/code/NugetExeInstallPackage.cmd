1f334c25933daf9fde1ef022e1ea5cb7df50d0fe
dingjb <dingjb.yahoo.com>
2014-08-05 10:56:53 -0400

    Nuget Boostrapper Exe

---
+++ eT/code/NugetExeInstallPackage.cmd
@@ -0,0 +1,5 @@
+Nuget.exe install .eT\eT\code\Shell\Shell\packages.config
+Nuget.exe install .eT\eT\code\Shell\Trading\packages.config
+Nuget.exe install .eT\eT\code\Shell\Modules\packages.config
+Nuget.exe install .eT\eT\code\Shell\DataGridWithColumnStyle\packages.config
+Nuget.exe install .eT\eT\code\Shell\p1\packages.config

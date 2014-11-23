70dfa119eeb82e79975e7a515d7372c8299f6bcb
dingjb <dingjb.yahoo.com>
2014-11-22 20:36:17 -0500

    VD

---
+++ eT/code/VertocalDepth/SelectingItemAttachedProperty.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VertocalDepth
+{
+   public class SelectingItemAttachedProperty
+    {
+        public static readonly DependencyProperty SelectingItemProperty = DependencyProperty.RegisterAttached(
+            "SelectingItem",
+            typeof(MySelectionType),
+            typeof(SelectingItemAttachedProperty),
+            new PropertyMetadata(default(MySelectionType), OnSelectingItemChanged));
+
+        public static MySelectionType GetSelectingItem(DependencyObject target)
+        {
+            return (MySelectionType)target.GetValue(SelectingItemProperty);
+        }
+
+        public static void SetSelectingItem(DependencyObject target, MySelectionType value)
+        {
+            target.SetValue(SelectingItemProperty, value);
+        }
+
+        static void OnSelectingItemChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var grid = sender as DataGrid;
+            if (grid == null || grid.SelectedItem == null)
+                return;
+
+            // Works with .Net 4.5
+            grid.Dispatcher.InvokeAsync(() => 
+            {
+                grid.UpdateLayout();
+                grid.ScrollIntoView(grid.SelectedItem, null);
+            });
+
+            // Works with .Net 4.0
+            grid.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                grid.UpdateLayout();
+                grid.ScrollIntoView(grid.SelectedItem, null);
+            }));
+        }
+    }
+
+   public class MySelectionType { }
+}
+//And here's the xaml snippet:
+
+//<Window ...
+//        xmlns:attachedProperties="clr-namespace:MyAttachedProperties">
+//    ...
+//        <DataGrid 
+//            attachedProperties:SelectingItemAttachedProperty.SelectingItem="{Binding MyViewModel.SelectingItem}">
+//            ...
+//        </DataGrid>
+//    </Grid>
+
